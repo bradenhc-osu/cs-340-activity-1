11bc3069a196bfe53f91b614c0b9f8e2bfc495e6
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-10-02 16:37:47 -0700

    devops: simplify the configuration setup

---
+++ src/config.js
@@ -0,0 +1,13 @@
+/**
+ * TODO: update the configuration in this file by changing the user, password, dbname, and onid fields:
+ * 
+ * Change all instances of "username" to your ONID and "XXXXX" to your MySQL DB password (the same one you use to log 
+ * in to phpMyAdmin)
+ */
+module.exports = {
+    host: 'classmysql.engr.oregonstate.edu',
+    user: 'cs340_username',
+    password: 'XXXXX',
+    dbname: 'cs340_username',
+    onid: 'username'
+};
