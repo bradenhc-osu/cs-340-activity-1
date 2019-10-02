2b4225ffb91e9fb61a0ec2a461e4663414e10487
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-10-02 16:43:28 -0700

    docs: fix typo and add clarification

--- src/routes/catalog.js
+++ src/routes/catalog.js
@@ -4,7 +4,10 @@ const { createViewContext } = require('../utils');
 const router = express.Router();
 
 /**
- * Route for listing part suppliers.
+ * Route for listing the catalog of parts.
+ * 
+ * This serves as an example of joining tables to produce more complex queries. You do not need to modify anything
+ * in this file.
  */
 router.get('/catalog', (req, res, next) => {
     req.db.query(
