abfa9c474c79782447e2c5525b39508e20a67e5b
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-10-02 16:40:51 -0700

    docs: add disclaimer about config in VCS

--- src/config.js
+++ src/config.js
@@ -3,6 +3,9 @@
  * 
  * Change all instances of "username" to your ONID and "XXXXX" to your MySQL DB password (the same one you use to log 
  * in to phpMyAdmin)
+ * 
+ * DISCLAIMER: it is not best practice to check configuration into source control (like GitHub). This is done here
+ * merely to simplify the setup process.
  */
 module.exports = {
     host: 'classmysql.engr.oregonstate.edu',
